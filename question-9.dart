0bab8dba0921f486a994e1bf27324355849c6a23
suheerthedev <khansuheer1@gmail.com>
2024-07-29 15:44:49 +0500

    Almost there with Question 9

---
+++ question-9.dart
@@ -0,0 +1,22 @@
+//Implement a function that checks if a given string is a palindrome.
+// Example:
+// Input: "radar"
+// Output: "radar" is a palindrome.
+
+void main() {
+  String string = 'rsuheerr';
+  List list = string.split('');
+
+  bool check = false;
+
+  List newList = list.reversed.toList();
+
+  for (int i = 0; i < list.length; i++) {
+    check = list[i] == newList[i];
+  }
+  if (check) {
+    print('$string is palindrome');
+  } else {
+    print('$string is not a palindrome');
+  }
+}
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,5 @@
+
+  //   print('$string is palindrome');
+  // } else {
+  //   print('$string is not a palindrome');
+  // }

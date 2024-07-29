57f73c2f6dd9937c2f7df91cbc380bcb71cfe4e7
suheerthedev <khansuheer1@gmail.com>
2024-07-29 16:00:48 +0500

    Completed Question 11

---
+++ question-11.dart
@@ -0,0 +1,13 @@
+//Write a program to display a pattern like a right angle triangle using an asterisk using loop.
+
+import 'dart:io';
+
+void main() {
+  int rows = 4;
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write("*");
+    }
+    print("");
+  }
+}
